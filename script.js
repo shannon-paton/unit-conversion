23bf8772ced8e561aa70928655baba1a42211f6e
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-17 17:45:10 -0400

    meters to feet 3 decimal places

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function updateValues() {
 	var metersInitialValue = document.getElementById('meters-initial-value')
 	metersInitialValue.innerText = numberToConvert.value
 }
+
+// convert meters to feet
+
+meters = 1
+var feet = meters * 3.280839895
+feet = feet.toFixed(3)
+console.log(feet)
+
