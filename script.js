d1dcced907e4cfa950ab4ae147c528a0c8f7ddfd
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-17 18:59:07 -0400

    refactored updateValues() and created updateLength()

--- script.js
+++ script.js
@@ -18,32 +18,35 @@ function convertFeetToMeters(feet) {
 }
 
 function updateValues() {
+	let inputNumber = parseFloat(numberToConvert.value)
+
+	if ( numberToConvert.value != "" ) { // if the string is not empty
+		updateLength(inputNumber) // update length (feet/meters) using inputted number
+
+	}
+
+	else {
+		updateLength(0) // else run function with 0 as input
+		
+	}
+
+}
+
+function updateLength(number) { // number is parameter
 
 	var metersInitialValue = document.getElementById('meters-initial-value')
 	var feetConvertedValue = document.getElementById('feet-converted-value') // gets span of converted value
 	var feetInitialValue = document.getElementById('feet-initial-value')
 	var metersConvertedValue = document.getElementById('meters-converted-value')
 
+	// gets span of initial value
+	metersInitialValue.innerText = number // updates text of value to the number typed in the top
+	var feet = convertMetersToFeet(number) // converts to feet
+	feetConvertedValue.innerText = feet // replace inner text with converted value
 
-	if ( numberToConvert.value != "" ) {
-		 // gets span of initial value
-		metersInitialValue.innerText = numberToConvert.value // updates text of value to the number typed in the top
-		var feet = convertMetersToFeet(parseFloat(numberToConvert.value)) // converts to feet
-		feetConvertedValue.innerText = feet // replace inner text with converted value
-
-		feetInitialValue.innerText = numberToConvert.value
-		var meters = convertFeetToMeters(parseFloat(numberToConvert.value))
-		metersConvertedValue.innerText = meters
-
-	}
-
-	else {
-		metersInitialValue.innerText = '0'
-		feetConvertedValue.innerText = '0.000'
-		feetInitialValue.innerText = '0'
-		metersConvertedValue.innerText = '0.000'
-	}
-
+	feetInitialValue.innerText = number
+	var meters = convertFeetToMeters(number)
+	metersConvertedValue.innerText = meters
 }
 
 
