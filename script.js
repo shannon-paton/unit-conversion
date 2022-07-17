0b05dc4d7081b66d1d7c1ffa1a58bf97dd4f3a8a
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-07-17 17:48:23 -0400

    made function to convert meters to feet

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var numberToConvert = document.getElementById('number-to-convert');
-numberToConvert.addEventListener('change', updateValues);
+numberToConvert.addEventListener('change', updateValues); //onchange, run the function
 
 function updateValues() {
 	var metersInitialValue = document.getElementById('meters-initial-value')
 	metersInitialValue.innerText = numberToConvert.value
+	var feetConvertedValue = document.getElementById('feet-converted-value')
+	console.log(feetConvertedValue)
 }
 
 // convert meters to feet
 
-meters = 1
-var feet = meters * 3.280839895
-feet = feet.toFixed(3)
-console.log(feet)
+function convertMetersToFeet(meters) {
+	var feet = meters * 3.280839895
+	feet = feet.toFixed(3)
+	console.log(feet)
+}
+
+
+
+
 
